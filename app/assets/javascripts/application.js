923c15d8e0e3cd08d3f2e69c1ea0f3d3d44dd13e
FuminoriSugawara <fuminori.sugawara@gmail.com>
2014-04-04 22:52:55 +0900

    add layout application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
-//= require highcharts/highcharts                                                           
-//= require highcharts/highcharts-more                                                         
-//= require highcharts/highstock
 //= require_tree .
 
