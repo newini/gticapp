78a0f6a9928d4380aa949fa1c32189fff00c0326
newini <chariskimec@gmail.com>
2018-08-23 15:26:06 +0900

    Fixed bugs and deleted unecessary files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,
     Settings.OmniAuth.facebook.app_id,
     Settings.OmniAuth.facebook.app_secret,
-    :scope => 'email,user_birthday,user_events'
+    #:scope => 'email,user_birthday,user_events'
+    :scope => 'email,user_birthday'
 end
