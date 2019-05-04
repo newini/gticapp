68846c21c385a243b3dc33902cf0db539afc7577
newini <chariskimec@gmail.com>
2019-05-04 23:17:15 +0900

    Modified facebook name display and commented out roman func in Members

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -10,23 +10,23 @@ module MembersHelper
   end
 
   def kana(kanji)
-    sentence = kanji
-    sentence = URI.encode(sentence)
-    key = 'dj0zaiZpPVR3TzJrbEJzRjRwTCZzPWNvbnN1bWVyc2VjcmV0Jng9OTg-'
-    base_url = 'http://jlp.yahooapis.jp/FuriganaService/V1/furigana'
-    req_url = "#{base_url}?sentence=#{sentence}&appid=#{key}"
-    response = Net::HTTP.get_response(URI.parse(req_url))
-    status = Hash.from_xml(response.body)
-    words = status["ResultSet"]["Result"]["WordList"]["Word"]
-    value = String.new
-    words.each do |word|
-      if word["Furigana"].nil?
-        value << word["Surface"]
-      else
-        value << word["Furigana"]
-      end
-    end
-    value
+    #sentence = kanji
+    #sentence = URI.encode(sentence)
+    #key = 'dj0zaiZpPVR3TzJrbEJzRjRwTCZzPWNvbnN1bWVyc2VjcmV0Jng9OTg-'
+    #base_url = 'http://jlp.yahooapis.jp/FuriganaService/V1/furigana'
+    #req_url = "#{base_url}?sentence=#{sentence}&appid=#{key}"
+    #response = Net::HTTP.get_response(URI.parse(req_url))
+    #status = Hash.from_xml(response.body)
+    #words = status["ResultSet"]["Result"]["WordList"]["Word"]
+    #value = String.new
+    #words.each do |word|
+    #  if word["Furigana"].nil?
+    #    value << word["Surface"]
+    #  else
+    #    value << word["Furigana"]
+    #  end
+    #end
+    #value
   end
 
 end
