c2cc836f8892066858eeafe71ac2bfc89ff775a2
Eunchong Kim <chariskimec@gmail.com>
2021-04-06 16:52:20 +0900

    Removed google api from config

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ require "csv"
 require "rails/all"
 # require "rails/test_unit/railtie"
 
-# Gmail API
-require 'google/apis/gmail_v1'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
