410eef612e7f3e6735b4bd4a0b151d1b04a1ee13
Eunchong Kim <chariskimec@gmail.com>
2021-05-05 20:13:13 +0900

    Added change locale button, translated home

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -9,4 +9,25 @@ module StaticPagesHelper
     return member.first_name_alphabet + " " + member.last_name_alphabet
   end
 
+  def select_locale
+    case params[:locale]
+    when 'en'
+      locale_now = ['English', 'en']
+      locale_array = [ ['Japanese', 'ja'] ]
+    when 'ja'
+      locale_now = ['Japanese', 'ja']
+      locale_array = [ ['English', 'en'] ]
+    end
+
+    content_tag(:li, class: "nav-item dropdown") {
+      concat content_tag(:a, href: '#', id: 'dropdowmLocale',class: "nav-link dropdown-toggle", data: {toggle: "dropdown"}, 'aria-haspopup': "true", 'aria-expanded': "false") {
+        content_tag(:b, locale_now[0], class: "caret")
+      }
+      concat content_tag(:div, class: "dropdown-menu", id: 'dropdowmLocale') {
+        locale_array.each do |locale|
+          concat link_to(locale[0], {locale: locale[1]}, class: 'dropdown-item')
+        end
+      }
+    }
+  end
 end
