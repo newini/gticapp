a423c2955104e3b09f70854ababccb17d59f2ee2
Eunchong Kim <chariskimec@gmail.com>
2021-07-29 11:37:22 +0900

    Added jquery in webpack application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,17 @@ Rails.start()
 Turbolinks.start()
 
 
+/* jQuery */
+// Use for ajax
+//import $ from 'jquery'; Not work
+// To solve Uncaught ReferenceError, $ is not defined
+var jQuery = require('jquery')
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+
 /* Bootstrap */
 import bootstrap from 'bootstrap/dist/js/bootstrap';
 // Bootstrap popover
